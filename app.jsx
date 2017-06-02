91454ca514663133452057d6cb128cea65981627
Ian8829 <ghjkghj@naver.com>
2017-06-03 08:19:56 +0900

    Decomposing

--- app.jsx
+++ app.jsx
@@ -1,35 +1,46 @@
+function Header(props) {
+    return (
+        <div className="header">
+          <h1>{props.title}</h1>
+        </div>
+    );
+}
+
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired,
+};
+
+function Player(props) {
+    return (
+        <div className="player">
+          <div className="player-name">
+              {props.name}
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> {props.score} </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+    );
+}
+
+Player.propTypes = {
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+};
+
 function Application(props) {
     return (
         <div className="scoreboard">
-            <div className="header">
-              <h1>{props.title}</h1>
-            </div>
-            <div className="players">
-              <div className="player">
-                <div className="player-name">
-                  Jim Hokins
-                </div>
-                <div className="player-score">
-                  <div className="counter">
-                    <button className="counter-action decrement"> - </button>
-                    <div className="counter-score"> 31 </div>
-                    <button className="counter-action increment"> + </button>
-                  </div>
-                </div>
-              </div>
+            <Header title={props.title}/>
 
-              <div className="player">
-                <div className="player-name">
-                  Andrew Chalkey
-                </div>
-                <div className="player-score">
-                  <div className="counter">
-                    <button className="counter-action decrement"> - </button>
-                    <div className="counter-score"> 33 </div>
-                    <button className="counter-action increment"> + </button>
-                  </div>
-                </div>
-              </div>
+            <div className="players">
+              <Player name="Jim Hoskins" score={31}/>
+              <Player name="Andrew Chalkey" score={35}/>
+
             </div>
         </div>
     );
