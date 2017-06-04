074bf8af0516b96be9f72b387f466f3c25752feb
Ian8829 <ghjkghj@naver.com>
2017-06-05 07:49:20 +0900

    stopwatch

--- app.jsx
+++ app.jsx
@@ -19,16 +19,38 @@ const PLAYERS = [
 let nextId = 4;
 
 const Stopwatch = React.createClass({
-   render: function () {
-       return (
-           <div className="stopwatch">
-               <h2>Stopwatch</h2>
-               <div className="stopwatch-time">0</div>
-               <button>Start</button>
-               <button>Reset</button>
-           </div>
-       );
-   }
+       getInitialState: function () {
+           return {
+               running: false
+           }
+       },
+
+       onStart: function () {
+           this.setState({ running: true });
+       },
+
+
+       onStop: function () {
+           this.setState({ running: false });
+       },
+
+       onReset: function () {
+
+       },
+
+       render: function () {
+           return (
+               <div className="stopwatch">
+                   <h2>Stopwatch</h2>
+                   <div className="stopwatch-time">0</div>
+                   { this.state.running ?
+                   <button onClick={this.onStop}>Stop</button> :
+                   <button onClick={this.onStart}>Start</button>
+                   }
+                   <button onClick={this.onReset}>Reset</button>
+               </div>
+           );
+       }
 });
 
 const AddPlayerForm = React.createClass({
